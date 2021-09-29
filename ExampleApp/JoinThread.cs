94b228410040b86deca310874b852fd60818043a
Beka_Kublashvili <bekakubla@gmail.com>
2021-09-29 15:43:38 +0400

    Init Thread Join

---
+++ ExampleApp/JoinThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ExampleApp
+{
+    public class JoinThread
+    {
+        public static void Main()
+        {
+            Console.WriteLine("main method start here");
+
+            Thread t1 = new Thread(JoinMethod);
+
+            t1.Start();
+            t1.Join();
+
+            Console.WriteLine("Main Method end here");
+
+        }
+        public static void JoinMethod()
+        {
+            Console.WriteLine("start join method");
+            Thread.Sleep(1000);
+            Console.WriteLine("end join method");
+        }
+    }
+
+}
